90413671399a5d9f7d4ebf2d11eda681d713627f
Brian <qed327@naver.com>
2020-06-23 17:00:54 +0900

    0623 재귀함수 작동 분석

---
+++ recursive.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+// 재귀함수 작동.. 스택을 통해 작동하는데 생각보다 오래 반복함
+var count = 0
+func recursive(input:Int) -> Int {
+    count += 1
+    if input < 1 {
+        print("count :",count)
+        return 2
+        
+    } else {
+        let i = recursive(input: input-1)*2+1
+        print("일단 :",count,"번째",i)
+        return recursive(input: input-1)*2+1
+    }
+    
+}
+
+let b = recursive(input: 3)
