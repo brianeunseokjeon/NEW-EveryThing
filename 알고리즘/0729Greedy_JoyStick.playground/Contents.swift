ecd01b4d9d13819434cd27df0b110a5bf5aa3042
전은석 <qed327@naver.com>
2020-07-30 18:42:18 +0900

    0730 Greedy_테스트5번완료못함

--- 알고리즘/0729Greedy_JoyStick.playground/Contents.swift
+++ 알고리즘/0729Greedy_JoyStick.playground/Contents.swift
@@ -33,8 +33,14 @@ func joyStickMoveCount(char:Character) -> Int {
         return 0
     }
 }
+var gg = "BCDFE"
+var hh = "AAAAA"
 var aa = "BBBAAAAAAAAAB"
 aa.count
+var bb = "BBAAAA"
+var cc = "BBAACCCAAAAAABBBB"
+cc.count
+
 func findSequenceMinIndexAndMaxIndex(str:String) -> (Int,Int) {
     var max = 0
     var index = -1
@@ -42,25 +48,26 @@ func findSequenceMinIndexAndMaxIndex(str:String) -> (Int,Int) {
     var count = 0
     for s in str {
         index += 1
-        if s != "A" {
-            if max < count {
-                max = count
-                minIndex = index - max
-            }
-            count = 0
-        } else {
+        
+        if s == "A" {
             count += 1
-//            if max < count {
-//                max = count
-//                minIndex = index - max
-//            }
+            if max <= count {
+                max = count
+                minIndex = index - max + 1
+            }
+        } else if s != "A" {
+            count = 0
         }
     }
     return (minIndex,minIndex+max-1)
 }
+//findSequenceMinIndexAndMaxIndex(str: gg)
+//findSequenceMinIndexAndMaxIndex(str: hh)
+//findSequenceMinIndexAndMaxIndex(str: aa)
+//findSequenceMinIndexAndMaxIndex(str: bb)
+//findSequenceMinIndexAndMaxIndex(str: cc)
 
-findSequenceMinIndexAndMaxIndex(str: aa)
-
+//AABBABBAA
 
 //aa.last
 //aa.lastIndex(of: aa.last!)
@@ -71,18 +78,20 @@ func solution(_ name:String) -> Int {
     let frontCount = findSequenceMinIndexAndMaxIndex(str: name).0
     let rearCount = name.count - 1 - findSequenceMinIndexAndMaxIndex(str: name).1
     
+    //조이스틱으로 오른쪽 왼쪽 이동횟수
     if frontCount == 0 && name.count == rearCount {
         count += name.count-1
     } else if frontCount == 0 {
          count += rearCount
-    } else if rearCount == 0 {
-        count += frontCount
     } else if frontCount > rearCount {
         count += rearCount * 2 + frontCount - 1
     } else {
         count += (frontCount - 1) * 2 + rearCount
     }
+    
+    // 전부 AAAA일경우 횟수 0
     let temp = count
+    // 조이스틱으로 위아래 이동횟수
     for x in name {
         count += joyStickMoveCount(char: x)
     }
@@ -91,15 +100,40 @@ func solution(_ name:String) -> Int {
     } else {
         return count
     }
-    
-
-    
 }
-//solution("BBAAA")
-solution("AAAAABAAAAB")
-//14
-13
+
+//모두다 같은경우
+solution("AAA") //0
+
+//끝에가 AAA
+solution("BBAAA") //3
+//앞에가 AAA
+solution("AAABB") //4
+//중간에 A
+solution("BBABB")//8
+//중간에 AAA 여러개있고, 왼쪽이 갯수가 더 많을경우
+solution("BBBBAAAAB")//10
+//중간에 AAA 여러개 있고, 오른쪽 갯수가 더 많을경우
+solution("BAAAAABBB") //7
+//A와 반복
+solution("ABABA") //5
+
+solution("ABABAB") //8
+
+
+//주어진 경우
+solution("JEROEN")//56
+solution("JAN")//23
+
+
+
+solution("BAABBAAAAAACCA")//17
+solution("AZAAAZ")//5
+
+6 + 4
+5 + 4
 
 
 
 print(1)
+//문제 4와 7이 같은 스타일 5번은 다른것과 다름.
