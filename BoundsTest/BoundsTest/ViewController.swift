9371191d975c40b9a1c24ae78ee235fb0f19a731
전은석 <qed327@naver.com>
2020-08-25 15:25:09 +0900

    변함없음

--- BoundsTest/BoundsTest/ViewController.swift
+++ BoundsTest/BoundsTest/ViewController.swift
@@ -21,8 +21,8 @@ class ViewController: UIViewController {
 //바운스는 자신만의 좌표계를 따름.. 즉-> 여기선 boundsView안에 무한한 좌표계가 있음. 근데 보여지는건 저 파란색 크기만큼 보여짐. 그래서 bounds를 이동시키면 x 100, y 100 으로 할경우, 파란색뷰의 실제 위치는 변하지 않은체, 그안에 있는 좌표계만 이동하게 되는것. 이것은 스크롤뷰와 관련이 깊음!
     @IBAction func buttonAction(_ sender: Any) {
         boundsView.bounds = CGRect(x: 0, y: 0, width: 200, height: 200)
-        print(boundsView.bounds,ylabel.bounds)
-        
+//        print(boundsView.bounds,ylabel.bounds)
+     
         
     }
 }
