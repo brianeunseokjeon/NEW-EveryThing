69e279ac196b8bb3f830602089ee8f4821382052
전은석 <qed327@naver.com>
2021-03-31 11:14:04 +0900

    카카오 코테  01 오픈채팅방

---
+++ 2019 KAKAO/01OpenChattingRoom.playground/Contents.swift
@@ -0,0 +1,51 @@
+import UIKit
+
+
+//중요 문제 포인트는 isEnter의 값이 넣을때는 무조건 true. 그리고 밑에서 사용할때 바꿔줘야함.
+func solution(_ record:[String]) -> [String] {
+    var userDic = [String:(name:String,isEnter:Bool)]()
+    var order:[String] = []
+    var result:[String] = []
+    for user in record {
+        let userInfo = user.split(separator: " ")
+        let userId = String(userInfo[1])
+        
+        if userInfo.first == "Enter" {
+            let userName = String(userInfo[2])
+            order.append(userId)
+            if userDic[userId] == nil {
+                userDic.updateValue((userName,true), forKey: userId)
+            } else {
+                userDic[userId]?.name = userName
+            }
+        } else if userInfo.first == "Leave" {
+            order.append(userId)
+            
+        } else {
+            let userName = String(userInfo[2])
+            userDic[userId]?.name = userName
+        }
+    }
+    
+    for userId in order {
+        let name = userDic[userId]!.name
+        let isEnter = userDic[userId]!.isEnter
+        
+        if isEnter {
+            result.append(name+"님이 들어왔습니다.")
+            userDic[userId]?.isEnter = false
+            
+        } else {
+            result.append(name+"님이 나갔습니다.")
+            userDic[userId]?.isEnter = true
+            
+        }
+    }
+    return result
+}
+
+
+let input = ["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"]
+
+solution(input)
+
